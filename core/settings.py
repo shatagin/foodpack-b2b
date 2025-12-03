2b179802181808a0ee3fb307afaa0a86609d17e1
shatagin <a.shatagin@gmail.com>
2025-12-03 16:53:50 +0300

    new main page test

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
