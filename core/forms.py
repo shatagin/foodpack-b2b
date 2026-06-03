67684143f806dffc291e4056e09344f0aaee94d8
shatagin <a.shatagin@gmail.com>
2026-06-03 15:37:53 +0300

    обновление формы заявк

--- core/forms.py
+++ core/forms.py
@@ -1,13 +1,34 @@
 from django import forms
 from .models import Request
 
-class RequestForm(forms.ModelForm):
-    class Meta:
-        model = Request
-        fields = ('name', 'phone', 'email', 'comment')
-        widgets = {
-            'name': forms.TextInput(attrs={'placeholder': 'Название вашей организации и имя'}),
-            'phone': forms.TextInput(attrs={'placeholder': '+7 (___) ___-__-__'}),
-            'email': forms.EmailInput(attrs={'placeholder': 'you@vashsite.ru'}),
-            'comment': forms.Textarea(attrs={'placeholder': 'Введите текст запроса: вид упаковки, объёмы.', 'rows': 4}),
-        }
+
+class RequestForm(forms.Form):
+    company_name = forms.CharField(
+        label='Компания',
+        max_length=255,
+        widget=forms.TextInput(attrs={'placeholder': 'Название организации'})
+    )
+    contact_person = forms.CharField(
+        label='Контактное лицо',
+        max_length=255,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'Имя контактного лица'})
+    )
+    phone = forms.CharField(
+        label='Телефон',
+        max_length=50,
+        widget=forms.TextInput(attrs={'placeholder': '+7 (___) ___-__-__'})
+    )
+    email = forms.EmailField(
+        label='E-mail',
+        required=False,
+        widget=forms.EmailInput(attrs={'placeholder': 'you@company.ru'})
+    )
+    comment = forms.CharField(
+        label='Комментарий',
+        required=False,
+        widget=forms.Textarea(attrs={
+            'placeholder': 'Укажите вид упаковки, объем, сроки и прочие требования',
+            'rows': 4,
+        })
+    )
